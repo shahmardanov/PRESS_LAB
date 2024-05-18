696802873933f7a5186152ec756cd99883242d1d
shamerdanov <shamil332211@list.ru>
2024-05-18 12:46:26 +0400

    init commit

---
+++ src/main/java/com/example/press_lab/PressLabApplication.java
@@ -0,0 +1,13 @@
+package com.example.press_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PressLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PressLabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/press_lab/PressLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.press_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PressLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
