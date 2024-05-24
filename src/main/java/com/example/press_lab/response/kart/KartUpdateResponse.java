4bacec27cc304a5b56db914863230b0ef723c96e
IlgarNiftaly <158113662+IlgarNiftaly@users.noreply.github.com>
2024-05-25 00:05:27 +0400

    Add response

---
+++ src/main/java/com/example/press_lab/response/kart/KartUpdateResponse.java
@@ -0,0 +1,24 @@
+package com.example.press_lab.response.kart;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Service
+@NoArgsConstructor
+@AllArgsConstructor
+public class KartUpdateResponse {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String imageUrl;
+    private Long fkNewsId;
+    private String description;
+    private LocalDateTime createdAt;
+
+}
---
+++ src/main/java/com/example/press_lab/response/news/NewsReadResponse.java
@@ -0,0 +1,31 @@
+package com.example.press_lab.response.news;
+
+import com.example.press_lab.enums.CategoryStatus;
+import com.example.press_lab.enums.NewsStatus;
+import com.example.press_lab.enums.SubCategoryStatus;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Service
+@NoArgsConstructor
+@AllArgsConstructor
+public class NewsReadResponse {
+
+
+    private Long id;
+    private String title;
+    private String content;
+    private String imageUrl;
+    private Long viewCount;
+    private NewsStatus status;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private CategoryStatus categoryStatus;
+    private SubCategoryStatus subCategoryStatus;
+
+}
