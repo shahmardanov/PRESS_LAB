7ca1c008b1e820bef660e6e0838bb4ed161aa61b
Huseyn <59554521+huseynyusif@users.noreply.github.com>
2024-05-25 04:28:17 +0400

    mapper

---
+++ src/main/java/com/example/press_lab/mapper/NewsMapper.java
@@ -0,0 +1,16 @@
+package com.example.press_lab.mapper;
+
+import com.example.press_lab.entity.News;
+import com.example.press_lab.request.NewsCreateRequest;
+import com.example.press_lab.response.NewsCardResponse;
+import com.example.press_lab.response.NewsReadResponse;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface NewsMapper {
+
+    NewsReadResponse toReadResponse(News news);
+    NewsCardResponse toCardResponse(News news);
+
+    News toCreateEntity(NewsCreateRequest newsCreateRequest);
+}
