7c16e8738d07bbc6443780efff315d98a5ebee2d
Orxan1827 <144376728+Orxan1827@users.noreply.github.com>
2024-07-07 08:14:13 -0700

    NewsController fixing

--- src/main/java/com/example/press_lab/config/WebConfig.java
+++ src/main/java/com/example/press_lab/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*") // Her yerden gelen isteklere izin verir
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(false);
